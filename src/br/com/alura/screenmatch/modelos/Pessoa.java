1623612a56ebfc2b7ed0682f118ae78ae2f74aaf
gabriel luna <gabriel.lunnaa7@gmail.com>
2024-10-31 21:40:45 -0300

    final commit

---
+++ src/br/com/alura/screenmatch/modelos/Pessoa.java
@@ -0,0 +1,18 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + nome + " (Idade: " + idade + " Anos)";
+    }
+
+}
+
